f34a6ffd106425af341d5af3efc85bf1e5134094
Micheal Callaghan <mehaul96@gmail.com>
2025-05-25 18:10:15 -0500

    fixed cookie on mobile

--- components/CookieConsent.tsx
+++ components/CookieConsent.tsx
@@ -25,7 +25,7 @@ export default function CookieConsent({ onConsent }: { onConsent: () => void }) 
   if (!show) return null;
 
   return (
-    <div className="fixed bottom-4 left-4 right-4 bg-gray-800 text-white p-4 rounded-xl shadow-lg flex justify-between items-center">
+    <div className="fixed bottom-4 left-4 right-4 bg-gray-800 text-white p-4 rounded-xl shadow-lg flex flex-col sm:flex-row sm:justify-between sm:items-cente gap-4">
       <span>I am using cookies to track basic site traffic. Is that cool?</span>
       <div className="space-x-2">
         <button onClick={handleAccept} className="bg-green-500 px-3 py-1 rounded">Accept</button>
