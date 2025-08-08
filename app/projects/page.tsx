e04afd51fc15918708c37aad35b5ac48bbc7f9a4
Micheal Callaghan <mehaul96@gmail.com>
2025-08-08 00:41:54 -0400

    updated projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,18 +10,34 @@ const projects = [
   {
     title: "Mjcal Fintech: Personal Finance",
     description: "A full-stack finance application built with Next.js, TypeScript, and functional APIs to Plaid, Appwrite, Sentry, and Dwolla.",
-    image: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&w=800&q=80",
+    image: "fintech-tn.png",
     tags: ["Next.js", "TypeScript", "Tailwind CSS", "APIs"],
     github: "https://github.com/mjcaldev/banking-app",
     demo: "https://mjcal-fintech-one.vercel.app/sign-in"
   },
   {
-    title: "Dentist Landing Page",
-    description: "Built with Vite, React, and Typescript, and AWS this is a simple, modern landing page with working pages and forms.",
-    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=800&q=80",
+    title: "Dentist Website",
+    description: "Built with Vite, React, and Typescript, and AWS this is a simple, modern site with working pages and forms.",
+    image: "dentist-tn.png",
     tags: ["React", "Vite", "AWS"],
     github: "https://github.com/mjcaldev/dentist-ctg",
     demo: "https://dentist-ctg.vercel.app/"
+  },
+  {
+    title: "Barbershop Multi-Tier Site",
+    description: "Built for a happy client, this site is slightly more advanced to address other common needs",
+    image: "barber-tn.png",
+    tags: ["React", "Next.js", "TypeScript"],
+    github: "https://github.com/mjcaldev/barbershop-client",
+    demo: "https://barbershop-client-ruddy.vercel.app/"
+  },
+  {
+    title: "Truthy Blog Site",
+    description: "This site has the essentials for any blogger and is currently in use as my career blog!",
+    image: "truthy-tn.png",
+    tags: ["React", "Next.js", "TypeScript"],
+    github: "https://github.com/mjcaldev/truthy",
+    demo: "https://www.truthy.io/"
   },
   // Add more projects as needed
 ]
@@ -51,11 +67,11 @@ export default function Projects() {
               transition={{ duration: 0.8, delay: index * 0.2 }}
               className="bg-card rounded-lg overflow-hidden shadow-lg"
             >
-              <div className="relative h-48 overflow-hidden">
+              <div className="relative aspect-[16/9] overflow-hidden">
                 <img
                   src={project.image}
                   alt={project.title}
-                  className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+                  className="w-full h-full object-contain transition-transform duration-300 hover:scale-105"
                 />
               </div>
               <div className="p-6">
